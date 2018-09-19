0c7bef5c11c2c043d87c9b0c3c4f5b850d00cd0f
200hp <qwertyqw8@mail.ru>
2018-09-19 20:06:54 +0300

    Love Triangle commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences= []){
+  	var count = 0;
+  	for(let i = 0; i < preferences.length; i++){
+  		let A = preferences[i] - 1;
+  		if (A == i) continue;
+  		let B = preferences[A] - 1;
+	    let C = preferences[B] - 1;
+	    count += i == C;
+  	}
+	return count/3;
 };
+//console.log(getLoveTrianglesCount([2, 3, 1]));
